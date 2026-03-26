024f91f5334e9a0706093c46760d6d7ffb5ccc17
uzairinayat <uzairinayat42@gmail.com>
2026-03-26 11:54:35 +0500

    1st commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Models } from './models/models';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'models', component: Models },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' }
+];
